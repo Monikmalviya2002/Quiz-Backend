26f500cab8d4e7d4d36fbb919b9a5ce5cc622b88
Monik Malviya <monikmalviya04gmail.com>
2025-11-27 16:32:57 +0530

    fix the routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 import express from "express";
 import connectDB from "./config/database.js"
 import dotenv from "dotenv";
+import cors from "cors"
 import QuestionRouter from "./routes/QuestionsRoutes.js";
 dotenv.config();
 
 const app = express();
+
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use("/api", QuestionRouter);
